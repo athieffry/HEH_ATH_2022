aaca476ef41210645b70c568b1f83b2b58662c5f
Axel Thieffry, PhD <axel.thieffry@gmail.com>
2022-10-26 10:58:28 +0200

    Add iris script and slides update

---
+++ scripts/iris.R
@@ -0,0 +1,15 @@
+# load the iris dataset (included in base R)
+data(iris)
+
+# inspect the iris dataset
+iris
+type(iris)
+str(iris)
+dim(iris)
+head(iris)
+tail(iris)
+summary(iris)
+colnames(iris)
+rownames(iris)
+View(iris)
+
